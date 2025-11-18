6b9f48a8a1cd037e3cc86292eceb8263059b1fcd
karpenkoa07 <karpenko.anastasiaa@gmail.com>
2025-11-18 21:38:32 +0300

    lab3.py

--- lab3.py
+++ lab3.py
@@ -4,6 +4,10 @@ import random
 import pygame
 
 alphabet = string.ascii_uppercase
+WIDTH = 1000
+HEIGHT = 500
+correctcodelength = 6
+
 
 pygame.mixer.init()
 pygame.mixer.music.load("am.mp3")
@@ -14,15 +18,15 @@ def animate():
     for star in stars:
         canvas.move(star, 0, 2)
         x1, y1, x2, y2 = canvas.coords(star)
-        if y1 > 500:
-            new_x = random.randint(0, 1000)
+        if y1 > HEIGHT:
+            new_x = random.randint(0, WIDTH)
             canvas.coords(star, new_x, 0, new_x + 2, 2)
     openwindow.after(30, animate)
 
 
 openwindow = tk.Tk()
 openwindow.title("Hello, User!")
-openwindow.geometry(f"{1000}x{500}")
+openwindow.geometry(f"{WIDTH}x{HEIGHT}")
 openwindow.resizable(False, False)
 
 canvas = tk.Canvas(openwindow, width=1000, height=500, bg="black")
@@ -30,8 +34,8 @@ canvas.pack(fill="both", expand=True)
 
 stars = []
 for _ in range(100):
-    x = random.randint(0, 1000)
-    y = random.randint(0, 500)
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
     star = canvas.create_oval(x, y, x, y, fill="white", outline="")
     stars.append(star)
 
@@ -55,7 +59,7 @@ def close_openwindow():
 
     def generate_key():
         number = entry_lbl.get()
-        if len(number) != 6:
+        if len(number) != correctcodelength:
             key_lbl.config(text="YOU ARE AN IMPOSTOR!", bg="red")
             txt_lbl.config(text="Try again! :(", bg="white")
             return
@@ -80,16 +84,16 @@ def close_openwindow():
     canvas.create_image(0, 0, image=bck_picture, anchor=tk.NW)
 
     entry_lbl = tk.Entry(root, font="Times 20 bold")
-    canvas.create_window(500, 50, window=entry_lbl)
+    canvas.create_window(WIDTH//2, 50, window=entry_lbl)
 
     button = tk.Button(root, text="GENERATE KEY", command=generate_key)
-    canvas.create_window(500, 300, window=button)
+    canvas.create_window(WIDTH//2, 300, window=button)
 
     key_lbl = tk.Label(root, text="", font=("Arial", 20))
-    canvas.create_window(500, 350, window=key_lbl)
+    canvas.create_window(WIDTH//2, 350, window=key_lbl)
 
     txt_lbl = tk.Label(root, text="", font=("Arial", 20))
-    canvas.create_window(500, 400, window=txt_lbl)
+    canvas.create_window(WIDTH//2, 400, window=txt_lbl)
 
     root.mainloop()
 
